06c813e070b272ab1af0a7e024450f5dfe65c918
GS SHAYKOT <shaykotkhan1916@gmail.com>
2026-03-09 17:28:36 +0600

    heat products dark mode enabled

--- app/components/(Home_Components)/HeatProduct.tsx
+++ app/components/(Home_Components)/HeatProduct.tsx
@@ -1,4 +1,4 @@
-import { HeatProductCard } from '@/app/components/(Home_Components)/HeatProductCard'
+import { HeatProductWrapper } from '@/app/components/(Home_Components)/HeatProductWrapper'
 import prisma from '@/lib/prisma'
 
 
@@ -12,19 +12,19 @@ export const HeatProduct = async () => {
             productImages: true,
             category: {
                 select: {
-                    name: true, 
+                    name: true,
                 }
             }
         },
         take: 4
     })
- 
+
     const newestProduct = heatProduct.reduce((latest, current) => {
         return new Date(current.createdAt) > new Date(latest.createdAt)
             ? current
             : latest
     })
- 
+
     const productsWithTags = heatProduct.map((product, index) => {
         const tags: string[] = []
 
@@ -33,18 +33,8 @@ export const HeatProduct = async () => {
 
         return { ...product, tags }
     })
-    
+
     return (
-        <div className="max-w-7xl px-4 mx-auto py-10">
-            <h2 className='text-4xl italic font-bold'>TRENDING<span className='text-second'> HEAT</span></h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-5">
-                {
-                    productsWithTags.map((product) => (
-                        // CARDS
-                        <HeatProductCard product={product} key={product.id} />
-                    ))
-                }
-            </div>
-        </div>
+        <HeatProductWrapper productsWithTags={productsWithTags} />
     )
 }
---
+++ app/components/(Home_Components)/HeatProductWrapper.tsx
@@ -0,0 +1,99 @@
+"use client"
+import { ArrowUpRight } from 'lucide-react'
+import { easeOut, motion } from "framer-motion";
+import { useSelector } from 'react-redux';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const HeatProductWrapper = ({ productsWithTags }: { productsWithTags: any[] }) => {
+
+    const theme = useSelector((state: any) => state.themeToggle.mode);
+
+    return (
+        <div className="max-w-7xl px-4 mx-auto py-10">
+            <h2 className={`text-4xl italic font-bold ${theme === "dark" ? "text-zinc-900" : "text-white"}`}>TRENDING<span className='text-second'> HEAT</span></h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-5">
+                {
+                    productsWithTags.map((product) => (
+                        // CARDS
+                        <div key={product.id} >
+                            <Link href={`/products/${product.id}`}>
+                                <motion.div
+                                    initial={{ opacity: 0, y: 40 }}
+                                    whileInView={{ opacity: 1, y: 0 }}
+                                    viewport={{ once: false, amount: 0.3 }}
+                                    transition={{
+                                        duration: 0.3,
+                                        ease: easeOut,
+                                    }}
+                                    whileHover={{
+                                        y: -6,
+                                        boxShadow: "0px 12px 24px rgba(0,0,0,0.15)",
+                                        borderColor: "#8fb300",
+                                    }}
+                                    className={`${theme === "dark"
+                                        ? "bg-white border-zinc-200 shadow-xs"
+                                        : "bg-zinc-900 border-transparent"
+                                        } border-2 overflow-hidden cursor-pointer group relative z-2`}
+                                >
+                                    {/* IMAGE */}
+                                    <div className="relative z-10 h-80 w-full overflow-hidden">
+                                        <div
+                                            className="h-full w-full"
+                                        >
+                                            <Image
+                                                src={product.productImages?.[0]?.imageUrl || "/placeholder.png"}
+                                                alt={product.name}
+                                                fill
+                                                className="object-cover"
+                                                sizes="(max-width: 768px) 100vw, 33vw"
+                                            />
+
+                                        </div>
+
+                                        <div className="w-5 h-5 border-b border-r absolute bottom-2 right-2 z-20 group-hover:border-second transition-all" />
+                                        <div className="w-5 h-5 border-b border-l absolute bottom-2 left-2 z-20 group-hover:border-second transition-all" />
+                                    </div>
+
+                                    {/* CONTENT */}
+                                    <div className="px-3 py-2 text-sm">
+                                        <div className="flex justify-between items-center">
+                                            <h3
+                                                className={` ${theme === "dark" ? "text-zinc-900" : "text-white"} font-semibold `}
+                                            >
+                                                {product.name}
+                                            </h3>
+
+                                            <div className="flex items-center gap-1">
+                                                <h3 className="text-second font-bold">
+                                                    &#36;{product.price}
+                                                </h3>
+                                                <ArrowUpRight
+                                                    className="text-zinc-400 group-hover:text-second"
+                                                    size={18}
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <p className="mt-1 text-sm text-gray-500">
+                                            {product.category?.name}
+                                        </p>
+                                    </div>
+
+                                    {
+                                        product.tags.map((tag: string, index: number) => (
+                                            <span
+                                                key={index} className=" bg-second text-zinc-900 text-xs p-2 absolute top-2 left-2 z-10 font-bold">
+                                                {tag}
+                                            </span>
+                                        ))
+                                    }
+                                </motion.div>
+                            </Link>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
