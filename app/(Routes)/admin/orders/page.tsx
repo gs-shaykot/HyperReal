c8b1bf787fb8007f18a85af2729839e8df92f97d
GS SHAYKOT <shaykotkhan1916@gmail.com>
2026-09-26 20:09:16 +0600

    feat: add Product, Analytics, Coupon, Order, Setting, and User pages with consistent styling

--- app/(Routes)/admin/orders/page.tsx
+++ app/(Routes)/admin/orders/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const ProductPage = () => {
+const OrderPage = () => {
     return (
         <div className="bg-main light:bg-white min-h-screen flex items-center justify-center">
             <h1 className="text-xl font-bold">
-                Products Page
+                Orders Page
             </h1>
         </div>
     )
 }
-export default ProductPage
+
+export default OrderPage
